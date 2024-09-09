162dd208e2a2ca14601d5e40bdba93472f087692
Oğuz Aytar <oguzaytar6637@gmail.com>
2024-09-09 15:34:39 +0300

    QR scan tekrar derlendi.

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: 'QR Code Scanner App', home: const AnaSayfa());
+    return const MaterialApp(title: 'QR Code Scanner App', home: AnaSayfa());
   }
 }
