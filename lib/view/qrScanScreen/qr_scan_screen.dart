3ff6477f880f5a9a39450a9e2a00e1e1e1edfb2c
Oğuz Aytar <oguzaytar6637@gmail.com>
2024-11-12 20:59:45 +0300

    adaaddas

--- lib/view/qrScanScreen/qr_scan_screen.dart
+++ lib/view/qrScanScreen/qr_scan_screen.dart
@@ -5,7 +5,14 @@ import 'package:flutter/material.dart';
 import 'package:qr_code_scanner/qr_code_scanner.dart';
 
 class QRScanScreen extends StatefulWidget {
-  const QRScanScreen({super.key});
+  const QRScanScreen({
+    super.key,
+    this.cutOutHeight,
+    this.cutOutWidth,
+  });
+
+  final double? cutOutHeight;
+  final double? cutOutWidth;
 
   @override
   State<QRScanScreen> createState() => _QRScanScreenState();
@@ -42,28 +49,30 @@ class _QRScanScreenState extends State<QRScanScreen> {
               bool flashStatus = snapshot.data ?? false;
               Icon flashIcon = _getFlashIcon(flashStatus);
               return IconButton(
-                  onPressed: () async {
-                    await controller?.toggleFlash();
-                    setState(() {
-                      flashStatus = !flashStatus;
-                      flashIcon = _getFlashIcon(flashStatus);
-                    });
-                  },
-                  icon: flashIcon);
+                onPressed: () async {
+                  await controller?.toggleFlash();
+                  setState(() {
+                    flashStatus = !flashStatus;
+                    flashIcon = _getFlashIcon(flashStatus);
+                  });
+                },
+                icon: flashIcon,
+              );
             },
           ),
           FutureBuilder(
             future: controller?.getFlashStatus(),
             builder: (context, snapshot) {
               return IconButton(
-                  onPressed: () async {
-                    await controller?.flipCamera();
-                    setState(() {});
-                  },
-                  icon: const Icon(
-                    Icons.flip_camera_ios_outlined,
-                    color: Colors.white,
-                  ));
+                onPressed: () async {
+                  await controller?.flipCamera();
+                  setState(() {});
+                },
+                icon: const Icon(
+                  Icons.flip_camera_ios_outlined,
+                  color: Colors.white,
+                ),
+              );
             },
           ),
         ],
@@ -72,32 +81,24 @@ class _QRScanScreenState extends State<QRScanScreen> {
     );
   }
 
-  Icon _getFlashIcon(bool flashstatusData) {
-    return flashstatusData
-        ? const Icon(
-            Icons.flash_on,
-            color: Colors.white,
-          )
-        : const Icon(
-            Icons.flash_off,
-            color: Colors.white,
-          );
+  Icon _getFlashIcon(bool flashStatus) {
+    return flashStatus ? const Icon(Icons.flash_on, color: Colors.white) : const Icon(Icons.flash_off, color: Colors.white);
   }
 
   Widget _buildQrView(BuildContext context) {
     var isPortrait = MediaQuery.of(context).orientation == Orientation.portrait;
     var isTablet = MediaQuery.of(context).size.shortestSide >= 600;
 
-    double cutOutHeight;
-    double cutOutWidth;
+    // Yükseklik ve genişlik kontrolü yapıyoruz
+    double cutOutHeight = widget.cutOutHeight ??
+        (isPortrait
+            ? (isTablet ? MediaQuery.sizeOf(context).height * 0.3 : MediaQuery.sizeOf(context).height * 0.17)
+            : (isTablet ? MediaQuery.sizeOf(context).height * 0.4 : MediaQuery.sizeOf(context).height * 0.25));
 
-    if (isPortrait) {
-      cutOutHeight = isTablet ? MediaQuery.sizeOf(context).height * 0.3 : MediaQuery.sizeOf(context).height * 0.17;
-      cutOutWidth = isTablet ? MediaQuery.sizeOf(context).width * 0.7 : MediaQuery.sizeOf(context).width * 0.95;
-    } else {
-      cutOutHeight = isTablet ? MediaQuery.sizeOf(context).height * 0.4 : MediaQuery.sizeOf(context).height * 0.25;
-      cutOutWidth = isTablet ? MediaQuery.sizeOf(context).width * 0.6 : MediaQuery.sizeOf(context).width * 0.8;
-    }
+    double cutOutWidth = widget.cutOutWidth ??
+        (isPortrait
+            ? (isTablet ? MediaQuery.sizeOf(context).width * 0.7 : MediaQuery.sizeOf(context).width * 0.95)
+            : (isTablet ? MediaQuery.sizeOf(context).width * 0.6 : MediaQuery.sizeOf(context).width * 0.8));
 
     return QRView(
       key: qrKey,
@@ -119,7 +120,7 @@ class _QRScanScreenState extends State<QRScanScreen> {
     log('${DateTime.now().toIso8601String()}_onPermissionSet $p');
     if (!p) {
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(content: Text('no Permission')),
+        const SnackBar(content: Text('No Permission')),
       );
     }
   }
@@ -135,8 +136,7 @@ class _QRScanScreenState extends State<QRScanScreen> {
         if (isScanning) {
           setState(() {
             result = scandata;
-
-            isScanning = false; // Tarama durumu bayrağını güncelle
+            isScanning = false;
           });
 
           Navigator.of(context).pop(result?.code ?? '');
